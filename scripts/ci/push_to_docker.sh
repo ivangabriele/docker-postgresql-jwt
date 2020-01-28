535493ba7f8b495ad8e93bd0dafeba7904f0bb68
Ivan Gabriele <ivan.gabriele@gmail.com>
2020-01-28 16:42:48 +0100

    fix(scripts): set missing IMAGE variable

--- scripts/ci/push_to_docker.sh
+++ scripts/ci/push_to_docker.sh
@@ -3,5 +3,7 @@
 # Exit when any command fails:
 set -e
 
+IMAGE="igabriele/postgresql-pgjwt:${VERSION}${VARIANT:+-${VARIANT}}"
+
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USER}" --password-stdin
 docker push "${IMAGE}"
